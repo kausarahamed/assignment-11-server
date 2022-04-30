3eb5e12e6be96ce2bf57c229ed3aad2059afc03a
kausarahamed <galibhasan517@gmail.com>
2022-04-30 14:50:03 +0600

    server compleate

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nlmz9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -17,7 +18,13 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const collection = client.db("test").collection("devices");
+    const cycleCollection = client.db("cyclewarehouse").collection("cycle");
+    app.get("/user", async (req, res) => {
+      const query = {};
+      const cursor = cycleCollection.find(query);
+      const users = await cursor.toArray();
+      res.send(users);
+    });
   } finally {
     // await client.close();
   }
@@ -25,7 +32,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Running Genius Server");
+  res.send("Running cycle Server");
 });
 
 app.listen(port, () => {
