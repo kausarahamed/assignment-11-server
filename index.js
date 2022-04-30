9431716630c0983f8f5e9ebffefeb9475778a0d7
kausarahamed <galibhasan517@gmail.com>
2022-04-30 12:07:22 +0600

    mongodb connect

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nlmz9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const collection = client.db("test").collection("devices");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Running Genius Server");
+});
+
+app.listen(port, () => {
+  console.log("Listening to port", port);
+});
